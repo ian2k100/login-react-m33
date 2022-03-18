7af3dbdbdff964fdcebda6cec399e540528973e2
ian2k100 <ianrmukoma@gmail.com>
2022-03-18 15:49:20 +0000

    Third Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { fetchRequest, tokenFetch } from './utils';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  useEffect(() => {
+    tokenFetch(setUser)
+  }, [])
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+    fetchRequest(setUser, username, email, password);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{user ? `Welcome ${user}` : "Please Login or Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <input onChange={(e) => setUsername(e.target.value)} placeholder="Username"/>
+        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password"/>
+      <button type="submit"> Submit </button> 
+      </form>
     </div>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,32 @@
+export const fetchRequest = async (setUser, username, email, password) => {
+    try {
+        const response = await fetch (`${process.env.REACT_APP_REST_API}user`, {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                password: password,
+            }),
+        });
+        const data = await response.json();
+        setUser(data.user);
+        localStorage.setItem("myToken", data.token);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const tokenFetch = async (setUser) => {
+    try {
+        const token = localStorage.getItem("myToken");
+        const response = await fetch(`${process.env.REACT_APP_REST_API}token`, {
+            method: "GET",
+            headers: { 'Authorization': `Bearer ${token}`}
+        });
+        const data = await response.json();
+        setUser(data.user)
+    } catch (error) {
+        console.log(error)
+    }
+}
